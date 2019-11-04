98d3fc9f3435edeabf4e2f5304cb1ba723963981
hanrichard <hxy.314@gmail.com>
2019-11-04 12:32:51 +1100

    add select and search func

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,94 +1,116 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Companyheader from './CompanyHeader';
-import CompanyPanel from './CompanyPanel';
-import CompanyStaffList from './CompanyStaffList';
-import CompanyModal from './CompanyModal';
-import CompanyEmpolyeeInfo from './CompanyEmpolyeeInfo';
-import Container from '@material-ui/core/Container';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Companyheader from "./CompanyHeader";
+import CompanyPanel from "./CompanyPanel";
+import CompanyStaffList from "./CompanyStaffList";
+import CompanyModal from "./CompanyModal";
+import CompanyEmpolyeeInfo from "./CompanyEmpolyeeInfo";
+import Container from "@material-ui/core/Container";
 
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 class Company extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			companyInfo: null,
-			show: false,
-			employee: {},
-			term: '',
-			select: 'lastName',
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      companyInfo: null,
+      show: false,
+      employee: {},
+      term: "",
+      select: "lastName"
+    };
+  }
 
-	onChangeInput = e => {
-		this.setState({
-			term: e.target.value,
-		});
-	};
+  onChangeInput = e => {
+    this.setState({
+      term: e.target.value
+    });
+  };
 
-	onChangeSelect = e => {
-		this.setState({
-			select: e.target.value,
-		});
-	};
+  onChangeSelect = e => {
+    this.setState({
+      select: e.target.value
+    });
+  };
 
-	showModal = id => {
-		const { employees } = this.props.companies;
-		const employee = employees.find(element => element.id === id);
+  showModal = id => {
+    const { employees } = this.props.companies;
+    const employee = employees.find(element => element.id === id);
 
-		this.setState({
-			show: true,
-			employee,
-		});
-	};
+    this.setState({
+      show: true,
+      employee
+    });
+  };
 
-	hideModal = () => {
-		this.setState({
-			show: false,
-			employee: {},
-		});
-	};
+  hideModal = () => {
+    this.setState({
+      show: false,
+      employee: {}
+    });
+  };
 
-	componentDidMount() {
-		this.props.onInitCompanies();
-	}
+  componentDidMount() {
+    this.props.onInitCompanies();
+  }
 
-	render() {
-		const { companies } = this.props;
-		const { employees, companyInfo } = companies;
-		const { term, show, employee, select } = this.state;
+  render() {
+    const { companies } = this.props;
+    const { employees, companyInfo } = companies;
+    const { term, show, employee, select } = this.state;
+    const name = this.state.select;
 
-		return this.props.companies.length === 0 ? (
-			<CircularProgress />
-		) : (
-			<Container maxWidth="lg">
-				<div className="Company__container">
-					<Companyheader info={companyInfo} />
-					<div className="Company__container">
-						<CompanyPanel
-							onChangeInput={this.onChangeInput}
-							onChangeSelect={this.onChangeSelect}
-							searchValue={term}
-							selectValue={select}
-						/>
-					</div>
-					<div className="Company__card">
-						<CompanyStaffList employees={employees} showModal={this.showModal} employee={employee} />
-					</div>
-					<div className="Company__modal">
-						<CompanyModal show={show} hideModal={this.hideModal}>
-							<CompanyEmpolyeeInfo employee={employee} />
-						</CompanyModal>
-					</div>
-				</div>
-			</Container>
-		);
-	}
+    const regex = new RegExp(this.state.term, "gi");
+    let updatedEmployees =
+      employees &&
+      employees
+        .sort(function(a, b) {
+          return a[name] > b[name] ? 1 : b[name] > a[name] ? -1 : 0;
+        })
+        .reduce((acc, item) => {
+          if (
+            (item && item.lastName.match(regex)) ||
+            (item && item.firstName.match(regex))
+          ) {
+            acc.push(item);
+          }
+          return acc;
+        }, []);
+
+    return this.props.companies.length === 0 ? (
+      <CircularProgress />
+    ) : (
+      <Container maxWidth="lg">
+        <div className="Company__container">
+          <Companyheader info={companyInfo} />
+          <div className="Company__container">
+            <CompanyPanel
+              onChangeInput={this.onChangeInput}
+              onChangeSelect={this.onChangeSelect}
+              searchValue={term}
+              selectValue={select}
+            />
+          </div>
+          <div className="Company__card">
+            <CompanyStaffList
+              employees={updatedEmployees}
+              showModal={this.showModal}
+              employee={employee}
+            />
+          </div>
+          <div className="Company__modal">
+            <CompanyModal show={show} hideModal={this.hideModal}>
+              <CompanyEmpolyeeInfo employee={employee} />
+            </CompanyModal>
+          </div>
+        </div>
+      </Container>
+    );
+  }
 }
 
 Company.propTypes = {
-	onInitCompanies: PropTypes.func,
+  onInitCompanies: PropTypes.func
 };
 
 export default Company;
