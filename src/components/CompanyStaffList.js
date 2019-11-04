8819992887784c924a8dd9583d33a836bbfe0962
hanrichard <hxy.314@gmail.com>
2019-11-04 12:58:46 +1100

    add selected state

--- src/components/CompanyStaffList.js
+++ src/components/CompanyStaffList.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import PropTypes from 'prop-types';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
 
-import CompanyStaffListItem from './CompanyStaffListItem';
+import CompanyStaffListItem from "./CompanyStaffListItem";
 
-const CompanyStaffList = ({ employees, showModal }) => (
-	<Grid container spacing={3}>
-		{employees &&
-			employees.map(employee => (
-				<Grid item xs={12} sm={4} key={employee.id}>
-					<CompanyStaffListItem employee={employee} showModal={() => showModal(employee.id)} />
-				</Grid>
-			))}
-	</Grid>
+const CompanyStaffList = ({ employees, showModal, employee }) => (
+  <Grid container spacing={3}>
+    {employees &&
+      employees.map(item => (
+        <Grid item xs={12} sm={4} key={item.id}>
+          <CompanyStaffListItem
+            employee={item}
+            showModal={() => showModal(item.id)}
+            select={item.id === employee.id ? "is-selected" : ""}
+          />
+        </Grid>
+      ))}
+  </Grid>
 );
 
 CompanyStaffList.propTypes = {
-	showModal: PropTypes.func,
-	employees: PropTypes.array,
+  showModal: PropTypes.func,
+  employees: PropTypes.array
 };
 
 export default CompanyStaffList;
