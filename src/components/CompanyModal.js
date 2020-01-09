6e6b098e344314073da83ddb154d6a54cdbb3e9f
richardhan <hxy.314@gmail.com>
2020-01-09 23:00:35 +1100

    feat(modal): refactor modal

--- src/components/CompanyModal.js
+++ src/components/CompanyModal.js
@@ -1,65 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
+import Modal from '@material-ui/core/Modal';
+import Paper from '@material-ui/core/Paper';
 
-import styled from 'styled-components';
+const containerStyles = {
+	position: 'relative',
+};
 
-const ModalWrapper = styled.div`
-	position: fixed;
-	z-index: 1;
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 100%;
-	overflow: auto;
-	background-color: rgba(0, 0, 0, 0.4);
+const styles = {
+	alignItems: 'center',
+	justifyContent: 'center',
+	display: 'flex',
+	position: 'fixed',
+	zIndex: 1,
+	left: 0,
+	top: 0,
+	right: 0,
+	bottom: 0,
+	overflow: 'auto',
+	backgroundColor: 'rgba(0, 0, 0, 0.4)',
+};
 
-	.Modal__container {
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		z-index: 0;
-		right: 0;
-		position: absolute;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		background-color: rgba(0, 0, 0, 0.4);
-	}
-
-	.Modal__container--inner {
-		background-color: white;
-		position: relative;
-	}
-
-	.Modal__button {
-		position: absolute;
-		right: 0;
-		top: 0;
-	}
-`;
+const buttonStyles = {
+	position: 'absolute',
+	right: 0,
+	top: 0,
+	zIndex: 2,
+};
 
 export const CloseButton = ({ hideModal }) => (
-	<button className="Modal__button" onClick={hideModal}>
-		X
+	<button type="button" style={buttonStyles} onClick={hideModal}>
+		x
 	</button>
 );
 
-const CompanyModal = ({ children, show, hideModal }) =>
-	show ? (
-		<ModalWrapper>
-			<div className="Modal__container" onClick={hideModal}>
-				<Container maxWidth="md">
-					<div className="Modal__container--inner">
-						<CloseButton hideModal={hideModal} />
-						{children}
-					</div>
-				</Container>
-			</div>
-		</ModalWrapper>
-	) : null;
+const CompanyModal = ({ children, show, hideModal }) => (
+	<Modal
+		aria-labelledby="simple-modal-title"
+		aria-describedby="simple-modal-description"
+		open={show}
+		style={styles}
+		BackdropProps={{
+			timeout: 500,
+		}}
+		onClose={hideModal}
+	>
+		<Paper style={containerStyles} elevation={3}>
+			<Container maxWidth="lg">
+				<CloseButton hideModal={hideModal} />
+				{children}
+			</Container>
+		</Paper>
+	</Modal>
+);
 
 CompanyModal.propTypes = {
 	children: PropTypes.node,
